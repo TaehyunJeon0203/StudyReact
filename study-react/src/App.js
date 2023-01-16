a732ad1d2633d93f941ea1a80adc6bb3f68713be
TaehyunJeon <jeontaehyun0203@gmail.com>
2023-01-16 14:26:51 +0000

    23.01.16(#6.2 useEffect)

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -1,11 +1,35 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  }
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("i run when counter changes");
+  }, [counter]);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when counter & keyword changes");
+  }, [counter, keyword]);
+  
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!!!!</h1>
-      <Button text={"hello"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
